58f2606ece9ae6ac824f3bad36af1d983073ca04
Izzan <muhammadizzanaquilla@gmail.com>
2026-04-27 22:07:28 +0700

    Pertemuan8

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-    <title>Bootstrap 4 Example</title>
+    <title>Tutorial Laravel - www.malasngoding.com</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
@@ -12,13 +12,11 @@
 </head>
 
 <body>
-
     <div class="container">
         <h3>www.malasngoding.com</h3>
         <p>Seri Tutorial Laravel Lengkap Dari Dasar</p>
         <p>Ini adalah view blog. ada di route blog.</p>
     </div>
-
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DosenController;
+use App\Http\Controllers\DosenController ;
+use App\Http\Controllers\PegawaiController ;
+use App\Http\Controllers\BlogController ;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,14 +12,21 @@ Route::get('/', function () {
 Route::get('halo', function () {
 	return "<h1>Halo, Selamat datang</h1> di tutorial laravel <i>www.malasngoding.com</i>";
 });
-
 Route::get('blog', function () {
-	return view('blog');
+    return view('blog');
 });
 
-Route::get('p5', function () {
-	return view('pertemuan5');
+Route::get('pert5', function () {
+    return view('pertemuan5');
 });
 
 Route::get('dosen', [DosenController::class, 'index']);
 Route::get('biodata', [DosenController::class, 'biodata']);
+
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+//blog
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
